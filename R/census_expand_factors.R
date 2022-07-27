10b0f93253692b7ab0fef4a642bc1a45f6aee7d0
Ben Taft <ben.taft@career2cradle.org>
2022-07-27 10:18:44 -0500

    much better factor expansion for querying the census api

--- R/census_expand_factors.R
+++ R/census_expand_factors.R
@@ -2,34 +2,20 @@
 
 #' Create a table of census variables from factors and row offsets
 #'
-#' @param .x the table of factors and row offsets
-#' @param .level_field the variable that names factor levels
-#' @param .offset_field the variable that gives row offsets
-#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> other factors without offsets
+#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> data frames with factors and offsets
+#' @param .offset_field the variable that gives row offsets, defaults to "offset"
 #'
 #' @return a table of census variables with their associated factors and rows
 #' @export
-census_expand_factors <- function(.x,
-                                  .level_field = "Level",
-                                  .offset_field = "Offset",
-                                  ...) {
-    .x %>%
-        tidyr::nest(
-            data = tidyselect::any_of(c(.level_field,
-                                        .offset_field))
-        ) %>%
-        purrr::pmap(
-            ~ tibble::tibble("{..1}" := ..2[[.level_field]],
-                             "{..1}{.offset_field}" := ..2[[.offset_field]])
-        ) %>%
-        tidyr::expand_grid(
-            !!!.,
-            ...
-        ) %>%
+census_expand_factors <- function(..., .offset_field = "offset"){
+    tidyr::expand_grid(...,
+                       .name_repair = ~ vctrs::vec_as_names(.,
+                                                            repair = "unique",
+                                                            quiet = TRUE)
+    ) %>%
+        dplyr::rowwise() %>%
         dplyr::mutate(
-            Index = rowSums(
-                dplyr::across(tidyselect::ends_with(.offset_field))
-            ),
+            Index = sum(dplyr::c_across(tidyselect::contains(.offset_field))),
             .keep = "unused"
         ) %>%
         dplyr::arrange(
